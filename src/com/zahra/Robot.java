dc0e7fbe87c3eacfb7496553231f029c8bb01d3d
Zahra <zahramousavi7090@gmail.com>
2020-11-27 18:09:13 +0330

    validation for robot with test

--- src/com/zahra/Robot.java
+++ src/com/zahra/Robot.java
@@ -6,29 +6,52 @@ public class Robot {
     private int y;
     private boolean initial;
     private String[] directions = new String[]{"NORTH", "EAST", "SOUTH", "WEST"};
-    private int directionPointer;
+    private int directionPointer = -1;
+
+    public Robot() {
+        this.initial = false;
+    }
 
     public void place(int x, int y, String f) {
-        this.x = x;
-        this.y = y;
+        if (x < 0 || x > 4 || y < 0 || y > 4) {
+            throw new IllegalArgumentException("enter x and y between [0,4]");
+        }
         for (int i = 0; i < directions.length; i++)
             if (this.directions[i].equals(f))
                 this.directionPointer = i;
-        initial = true;
+        if (this.directionPointer == -1)
+            throw new IllegalArgumentException("enter valid direction.");
+        this.x = x;
+        this.y = y;
+        this.initial = true;
     }
 
     public void move() {
-        if (directions[directionPointer].equals("NORTH"))
+        if (!this.initial)
+            throw new IllegalArgumentException("you should enter PLACE, at the first");
+
+        if (directions[directionPointer].equals("NORTH")) {
+            if (this.y == 4)
+                throw new IllegalStateException("robot can't move.y will be out of range [0,4]");
             this.y++;
-        if (directions[directionPointer].equals("SOUTH"))
+        } else if (directions[directionPointer].equals("SOUTH")) {
+            if (this.y == 0)
+                throw new IllegalStateException("robot can't move.y will be out of range [0,4]");
             this.y--;
-        if (directions[directionPointer].equals("EAST"))
+        } else if (directions[directionPointer].equals("EAST")) {
+            if (this.x == 4)
+                throw new IllegalStateException("robot can't move.x will be out of range [0,4]");
             this.x++;
-        if (directions[directionPointer].equals("WEST"))
+        } else if (directions[directionPointer].equals("WEST")) {
+            if (this.x == 0)
+                throw new IllegalStateException("robot can't move.x will be out of range [0,4]");
             this.x--;
+        }
     }
 
     public void right() {
+        if (!this.initial)
+            throw new IllegalArgumentException("you should enter PLACE, at the first");
         if (this.directionPointer == directions.length - 1)
             directionPointer = 0;
         else
@@ -36,6 +59,8 @@ public class Robot {
     }
 
     public void left() {
+        if (!this.initial)
+            throw new IllegalArgumentException("you should enter PLACE, at the first");
         if (this.directionPointer == 0)
             this.directionPointer = directions.length - 1;
         else
@@ -43,6 +68,8 @@ public class Robot {
     }
 
     public String report() {
+        if (!this.initial)
+            throw new IllegalArgumentException("you should enter PLACE, at the first");
         return x + "," + y + "," + directions[directionPointer];
     }
 
--- test/com/zahra/RobotTest.java
+++ test/com/zahra/RobotTest.java
@@ -2,6 +2,7 @@ package com.zahra;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -14,44 +15,209 @@ public class RobotTest {
     }
 
     @Test
-    public void test1(){
-        robot.place(0,0,"NORTH");
-        String result=robot.report();
-        assertEquals(result,"0,0,NORTH");
+    public void place_with_correct_input() {
+        robot.place(0, 0, "NORTH");
+        String result = robot.report();
+        assertEquals(result, "0,0,NORTH");
     }
 
-    @Test
-    public void test2(){
-        robot.place(0,0,"NORTH");
+    @Test(expected = IllegalArgumentException.class)
+    public void place_with_x_less_than_zero() {
+        robot.place(-1, 0, "NORTH");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void place_with_x_more_than_four() {
+        robot.place(5, 0, "NORTH");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void place_with_y_less_than_zero() {
+        robot.place(0, -1, "NORTH");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void place_with_y_more_than_four() {
+        robot.place(0, 5, "NORTH");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void place_with_invalid_direction() {
+        robot.place(0, 0, "invalid");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void call_move_before_place_first_time() {
         robot.move();
-        String result=robot.report();
-        assertEquals(result,"0,1,NORTH");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void move_y_fewer_than_range() {
+        robot.place(0, 0, "SOUTH");
+        robot.move();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void move_y_more_than_range() {
+        robot.place(0, 4, "NORTH");
+        robot.move();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void move_x_fewer_than_range() {
+        robot.place(0, 0, "WEST");
+        robot.move();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void move_x_more_than_range() {
+        robot.place(4, 0, "EAST");
+        robot.move();
     }
 
     @Test
-    public void test3(){
-        robot.place(0,0,"NORTH");
-        robot.left();
-        String result=robot.report();
-        assertEquals(result,"0,0,WEST");
+    public void move_y_correct_range_more_than_zero() {
+        robot.place(0, 0, "NORTH");
+        robot.move();
+        String result = robot.report();
+        assertEquals(result, "0,1,NORTH");
     }
 
     @Test
-    public void test4(){
-        robot.place(0,0,"NORTH");
+    public void move_y_correct_range_less_than_four() {
+        robot.place(0, 4, "SOUTH");
+        robot.move();
+        String result = robot.report();
+        assertEquals(result, "0,3,SOUTH");
+    }
+
+    @Test
+    public void move_x_correct_range_more_than_zero() {
+        robot.place(0, 0, "EAST");
+        robot.move();
+        String result = robot.report();
+        assertEquals(result, "1,0,EAST");
+    }
+
+    @Test
+    public void move_x_correct_range_less_than_four() {
+        robot.place(4, 0, "WEST");
+        robot.move();
+        String result = robot.report();
+        assertEquals(result, "3,0,WEST");
+    }
+
+    @Test
+    public void right_rotate_north_to_east() {
+        robot.place(0, 0, "NORTH");
         robot.right();
-        String result=robot.report();
-        assertEquals(result,"0,0,EAST");
+        String result = robot.report();
+        assertEquals(result, "0,0,EAST");
     }
 
     @Test
-    public void test5(){
-        robot.place(1,2,"EAST");
+    public void right_rotate_east_to_south() {
+        robot.place(0, 0, "EAST");
+        robot.right();
+        String result = robot.report();
+        assertEquals(result, "0,0,SOUTH");
+    }
+
+    @Test
+    public void right_rotate_south_to_west() {
+        robot.place(0, 0, "SOUTH");
+        robot.right();
+        String result = robot.report();
+        assertEquals(result, "0,0,WEST");
+    }
+
+    @Test
+    public void right_rotate_west_to_north() {
+        robot.place(0, 0, "WEST");
+        robot.right();
+        String result = robot.report();
+        assertEquals(result, "0,0,NORTH");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void call_right_before_place_first_time() {
+        robot.right();
+    }
+
+    @Test
+    public void left_rotate_north_to_west() {
+        robot.place(0, 0, "NORTH");
+        robot.left();
+        String result = robot.report();
+        assertEquals(result, "0,0,WEST");
+    }
+
+    @Test
+    public void left_rotate_west_to_south() {
+        robot.place(0, 0, "WEST");
+        robot.left();
+        String result = robot.report();
+        assertEquals(result, "0,0,SOUTH");
+    }
+
+    @Test
+    public void left_rotate_south_to_east() {
+        robot.place(0, 0, "SOUTH");
+        robot.left();
+        String result = robot.report();
+        assertEquals(result, "0,0,EAST");
+    }
+
+    @Test
+    public void left_rotate_east_to_north() {
+        robot.place(0, 0, "EAST");
+        robot.left();
+        String result = robot.report();
+        assertEquals(result, "0,0,NORTH");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void call_left_before_place_first_time() {
+        robot.left();
+    }
+
+    @Test
+    public void correct_report() {
+        robot.place(3, 2, "WEST");
+        String result = robot.report();
+        assertEquals(result, "3,2,WEST");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void call_report_before_place_first_time() {
+        robot.report();
+    }
+
+    @Test
+    public void hybrid_test() {
+        robot.place(1, 2, "EAST");
         robot.move();
         robot.move();
         robot.left();
         robot.move();
-        String result=robot.report();
-        assertEquals(result,"3,3,NORTH");
+        String result = robot.report();
+        assertEquals(result, "3,3,NORTH");
+    }
+
+    @Test
+    public void hybrid_test_with_more_place() {
+        robot.place(0, 0, "EAST");
+        robot.move();
+        robot.move();
+        robot.move();
+        robot.right();
+        robot.place(0, 4, "EAST");
+        robot.move();
+        robot.move();
+        robot.move();
+        robot.right();
+        String result = robot.report();
+        assertEquals(result, "3,4,SOUTH");
+
     }
 }
