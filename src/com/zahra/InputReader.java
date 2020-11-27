73c5f1940d89ae1d554ac26cd342500810d25c26
Zahra <zahramousavi7090@gmail.com>
2020-11-27 19:00:41 +0330

    handle exception in InputReader

--- src/com/zahra/InputReader.java
+++ src/com/zahra/InputReader.java
@@ -13,23 +13,27 @@ public class InputReader {
 
     public void enterInput() {
         while (true) {
-            String nextLine = scanner.nextLine();
-            if (nextLine.equals("REPORT")) {
-                String report = robot.report();
-                System.out.println(report);
-                break;
-            }
-            if (nextLine.equals("MOVE"))
-                robot.move();
-            if (nextLine.equals("LEFT"))
-                robot.left();
-            if (nextLine.equals("RIGHT"))
-                robot.right();
-            if (nextLine.startsWith("PLACE")) { //PLACE 0,0,NORTH
-                String[] location = nextLine.split(" ")[1].split(",");
-                robot.place(Integer.parseInt(String.valueOf(location[0])),
-                        Integer.parseInt(String.valueOf(location[1])),
-                        location[2]);
+            try {
+                String nextLine = scanner.nextLine();
+                if (nextLine.equals("REPORT")) {
+                    String report = robot.report();
+                    System.out.println(report);
+                    break;
+                } else if (nextLine.equals("MOVE"))
+                    robot.move();
+                else if (nextLine.equals("LEFT"))
+                    robot.left();
+                else if (nextLine.equals("RIGHT"))
+                    robot.right();
+                else if (nextLine.startsWith("PLACE")) { //PLACE 0,0,NORTH
+                    String[] location = nextLine.split(" ")[1].split(",");
+                    robot.place(Integer.parseInt(String.valueOf(location[0])),
+                            Integer.parseInt(String.valueOf(location[1])),
+                            location[2]);
+                } else
+                    throw new IllegalArgumentException("incorrect input.");
+            } catch (IllegalArgumentException | IllegalStateException ex) {
+                System.out.println(ex.getMessage() + "\nplay again!");
             }
         }
     }
