a2d4e418c5f7d858dba6ae34b378868680021597
Zahra <zahramousavi7090@gmail.com>
2020-11-28 18:24:48 +0330

    handle exception of checkPlaceOrder method in InputReader

--- src/com/zahra/InputReader.java
+++ src/com/zahra/InputReader.java
@@ -45,18 +45,26 @@ public class InputReader {
         if (firstSplitWithSpace.length == 1)
             throw new ArrayIndexOutOfBoundsException("initial position after PLACE order");
         String[] location = firstSplitWithSpace[1].split(",");
-        robot.place(Integer.parseInt(String.valueOf(location[0])),
-                Integer.parseInt(String.valueOf(location[1])),
-                location[2]);
+        if (location.length != 3)
+            throw new IllegalArgumentException("enter correct position,for example:  place 2,3,west");
+        int x, y;
+        try {
+            x = Integer.parseInt(String.valueOf(location[0]));
+            y = Integer.parseInt(String.valueOf(location[1]));
+        } catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("x and y must be number ");
+        }
+        String direction = location[2];
+        robot.place(x, y, direction);
     }
 
-    private String gameGuide(){
-        String gameGuide="please select your operation as follows:\n" +
+    private String gameGuide() {
+        String gameGuide = "please select your operation as follows:\n" +
                 "\"move\" for move Robot in assigned direction\n" +
-                "\"left\" for turn Robot to left\n"+
-                "\"right\" for turn Robot to right\n"+
-                "\"report\" for see Robot position and direction\n"+
-                "\"exit\" for quit Robot Game \n"+
+                "\"left\" for turn Robot to left\n" +
+                "\"right\" for turn Robot to right\n" +
+                "\"report\" for see Robot position and direction\n" +
+                "\"exit\" for quit Robot Game \n" +
                 "\"place\" for put robot in one position,\n" +
                 "write this command in this format: place x,y,direction, \n" +
                 "then Robot placed in x,y,direction position,\n" +
